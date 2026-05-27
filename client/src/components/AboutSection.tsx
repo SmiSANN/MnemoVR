eb141e45dd07b966dc518e0d735557b7ddc4e36a
smi <picvrc@local>
2026-05-28 02:37:05 +0900

    release: v1.0.0

--- client/src/components/AboutSection.tsx
+++ client/src/components/AboutSection.tsx
@@ -7,7 +7,7 @@ export function AboutSection() {
         {strings.about.title}
       </h3>
       <p className="text-xs text-slate-300">
-        Version 0.2.6 - MnemoVR VRChat Photo Album & World Ranking
+        Version 1.0.0 - MnemoVR VRChat Photo Album & World Ranking
       </p>
     </section>
   );
